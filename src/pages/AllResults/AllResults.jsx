3ded11eeb7383833b5aac60b37b261b35e4c9946
Sumer Bhaidani <sumerbhaidani1@gmail.com>
2025-03-20 13:07:28 -0600

    GET All Results Complete

--- src/pages/AllResults/AllResults.jsx
+++ src/pages/AllResults/AllResults.jsx
@@ -1,13 +1,23 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./AllResults.scss";
 
-function AllResults() {
+function AllResults({ baseUrl }) {
+  const [allSurveys, setAllSurveys] = useState([]);
   async function getAllResults() {
     try {
-      const response = await axios.get();
+      const response = await axios.get(`${baseUrl}/survey`);
+      setAllSurveys(response.data);
+      console.log(response.data);
     } catch (error) {
       console.error("Unable to retrieve all results", error);
     }
   }
+
+  useEffect(() => {
+    getAllResults();
+  }, []);
   return (
     <div className="all-results">
       <h2 className="all-results__header">Past evaluations</h2>
@@ -15,8 +25,27 @@ function AllResults() {
         Attached below are results from all your past evaluations. Please click
         a page below to view the exercises
       </p>
-      <div className="all-results__each">
-        <h4 className="all-results__date">created_at</h4>
+      <div className="all-results__categories">
+        <h4 className="all-results__date">Date Evaluated</h4>
+      </div>
+      <div className="all-results__full">
+        {allSurveys.map((each) => {
+          return (
+            <div className="all-results__each" key={each.survey_id}>
+              <h4 className="all-results__date">
+                {new Date(each.created_at).toDateString()}
+              </h4>
+              <p className="all-results__redirect">
+                <Link
+                  to={`/evaluation/${each.survey_id}`}
+                  className="all-results__link"
+                >
+                  Open Exercise List
+                </Link>
+              </p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
