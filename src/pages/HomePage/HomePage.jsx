639dd10c7e476aaea88a19007be014789497e228
Sumer Bhaidani <sumerbhaidani1@gmail.com>
2025-04-17 21:47:42 -0600

    Mobile Content Add

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,5 @@
 import "./HomePage.scss";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 function HomePage() {
   return (
@@ -32,11 +32,38 @@ function HomePage() {
           <li className="home-page__guide-steps">
             Recieve a list of exercises tailored as per your evalution{" "}
           </li>
-          <p className="home-page__guide-detail">
+          <p className="home-page__guide-detail home-page--last-detail">
             Click on Past Evaluations to view your exercise lists at your
             convenience
           </p>
         </ol>
+      </section>
+      <section className="home-page__stat">
+        <h3 className="home-page__stat-data">
+          Ankle injuries don't just heal and disappear. Up to 40% of people
+          experience recurring sprains, chronic pain, or long-term instability
+          after their first injury.
+        </h3>
+        <a
+          href="https://www.ncbi.nlm.nih.gov/books/NBK560619/#:~:text=Ankle%20sprains%20are%20among%20the,the%20likelihood%20of%20further%20complications."
+          target="_blank"
+          className="home-page__stat-source"
+        >
+          - Mugno and Constant (2023)
+        </a>
+      </section>
+      <section className="home-page__cta">
+        <h4 className="home-page__explanation">
+          Don't let stiffness sneak up on you. Your mobility depends on strong,
+          flexible ankles.
+        </h4>
+        <h4 className="home-page__explanation">
+          Click below to create an account and take a quick, free 2-minute
+          evaluation today!
+        </h4>
+        <Link to="/questionnaire" className="home-page__redirect">
+          Click here to get started!
+        </Link>
       </section>
       {/* <h2 className="home-page__stat">
           Every year, approximately 2 million ankle injuries are reported in the
@@ -65,16 +92,6 @@ function HomePage() {
         >
           - Mugno and Constant (2023)
         </a> */}
-
-      <h4 className="home-page__explanation">
-        Don't let stiffness sneak up on you. Your mobility depends on strong,
-        flexible ankles. <br />
-        Click below to create an account and take a quick, free 2-minute
-        evaluation today!
-      </h4>
-      <Link to="/questionnaire" className="home-page__redirect">
-        Click here to get started!
-      </Link>
     </main>
   );
 }
