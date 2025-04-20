864dae67b0c02185889c048166db79b9206b87aa
Sumer Bhaidani <sumerbhaidani1@gmail.com>
2025-04-19 22:29:05 -0600

    Security Fix

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,7 +7,7 @@ function HomePage({ baseUrl }) {
   async function serverActivate() {
     try {
       const response = await axios.get(`${baseUrl}/`);
-      console.log(response);
+      console.log(response.data);
     } catch (error) {
       console.error(
         "Initial connection with server unsuccessful, please try again: ",
