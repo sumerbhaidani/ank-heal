6236bb41e20c99444942aa4c036c6159832b6521
Sumer Bhaidani <sumerbhaidani1@gmail.com>
2025-04-23 10:20:53 -0600

    Monthly Layout Complete

--- src/components/PriceCards/PriceCards.jsx
+++ src/components/PriceCards/PriceCards.jsx
@@ -5,7 +5,7 @@ function PriceCards() {
     <section className="price-card">
       <div className="price-card__single">
         <h3 className="price-card__product-name">Monthly Subscription</h3>
-        <h4 className="price-card__price">$12.99 / month</h4>
+        <h4 className="price-card__price">$14.99 / month</h4>
         <ul className="price-card__product-perks">
           <li className="price-card__product-perk-single">
             Unlimited Evaluations
@@ -19,10 +19,35 @@ function PriceCards() {
           <li className="price-card__product-perk-single">Recovery Tips</li>
           <li className="price-card__product-perk-single">Email Support</li>
         </ul>
+        <form
+          action="/create-checkout-session"
+          method="POST"
+          className="price-card__form"
+        >
+          <button className="price-card__button">Continue</button>
+        </form>
+        <p className="price-card__payment-redirect">
+          You will be redirected to another page where you can securely enter
+          your payment details
+        </p>
       </div>
       <div className="price-card__single">
         {" "}
         <h3 className="price-card__product-name">Yearly Subscription</h3>
+        <h4 className="price-card__price">$130 / year</h4>
+        <ul className="price-card__product-perks">
+          <li className="price-card__product-perk-single">
+            Unlimited Evaluations
+          </li>
+          <li className="price-card__product-perk-single">
+            Tailored Exercise Lists
+          </li>
+          <li className="price-card__product-perk-single">
+            Access to Your Past Evaluations
+          </li>
+          <li className="price-card__product-perk-single">Recovery Tips</li>
+          <li className="price-card__product-perk-single">Email Support</li>
+        </ul>
       </div>
     </section>
   );
