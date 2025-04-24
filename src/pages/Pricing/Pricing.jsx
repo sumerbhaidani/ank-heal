d6f55debba3ae86fb4d86cda2f1e6f535252b379
Sumer Bhaidani <sumerbhaidani1@gmail.com>
2025-04-23 21:27:27 -0600

    Pricing Page Styling Except Cards Complete

--- src/pages/Pricing/Pricing.jsx
+++ src/pages/Pricing/Pricing.jsx
@@ -1,10 +1,13 @@
 import PriceCards from "../../components/PriceCards/PriceCards.jsx";
-
+import "./Pricing.scss";
 function Pricing() {
   return (
     <section className="pricing">
-      <h2 className="pricing">AnkHeal Subscription Packages</h2>
-      <h3 className="pricing__cta">Step into strength here!</h3>
+      <h2 className="pricing__header">AnkHeal Subscription Packages</h2>
+      <h3 className="pricing__detail">
+        The choice is yours, each plan comes with a 7-day free trial so you can
+        see the value before you pay. No risk, just results.
+      </h3>
       <PriceCards />
       <p className="pricing__payment-redirect">
         You will be redirected to another page where you can securely enter your
