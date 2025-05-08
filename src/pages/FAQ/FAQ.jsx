de2e1f2cd58bb6622c2e3f4e4102d3fa6f0ef78a
Sumer Bhaidani <sumerbhaidani1@gmail.com>
2025-05-08 07:04:04 -0600

    Expand Add for All Questions - Make Individual handleDisplayFunctions for Each

--- src/pages/FAQ/FAQ.jsx
+++ src/pages/FAQ/FAQ.jsx
@@ -24,86 +24,143 @@ function FAQ() {
           principles.
         </h4>
       ) : null}
-      <h3 className="faq__question-text">
-        Is AnkHeal only for ankle recovery?
-      </h3>
-      <h4 className="faq__answer">
-        While AnkHeal focuses on ankle rehabilitation, many of the exercises
-        also support surrounding muscles and joints (calves, knees, etc.) which
-        can benefit overall mobility and athletic performance. The health of
-        surrounding muscles are taken into consideration when your exercises are
-        recommended.
-      </h4>
-      <h3 className="faq__question-text">How are the exercises selected?</h3>
-      <h4 className="faq__answer">
-        Your responses in the evaluation form are used to match you with a
-        tailored list of recovery-focused exercises. The exercises are selected
-        from our database based on research, recovery best practices, and
-        real-world testing to support your progress safely and effectively.{" "}
-      </h4>
-      <p className="faq__answer-warning">
-        ⚠️ If any exercise causes discomfort stop immediately or switch to a
-        gentler variation
-      </p>
-      <h3 className="faq__question-text">
-        What does the subscription provide?
-      </h3>
-      <ul className="faq__answer-list">
-        <li className="faq__answer-single-point">
-          Unlimited access to personalized ankle recovery plans
-        </li>
-        <li className="faq__answer-single-point">
-          A 7-day free trial for all subscription options
-        </li>
-        <li className="faq__answer-single-point">
-          Discounted yearly plan with early access to new features
-        </li>
-      </ul>
-      <Link to="/pricing" className="faq__plan-link">
-        See subscription plans
-      </Link>
+      <div className="faq__question" onClick={handleDisplayAnswer}>
+        {" "}
+        <h3 className="faq__question-text">
+          Is AnkHeal only for ankle recovery?
+        </h3>
+        <p className="faq__question-expand">+</p>
+      </div>
 
-      <h3 className="faq__question-text">
-        If I cancel my subscription, will I lose everything?
-      </h3>
-      <h4 className="faq__answer">
-        You won't be able to take new evaluations, but you will have access to
-        your past exercise plans.
-      </h4>
-      <h3 className="faq__question-text">
-        Can I cancel my subscription anytime?
-      </h3>
-      <h4 className="faq__answer">
-        Yes, you can cancel from your account settings at any time. Email us if
-        you run into any issues.
-      </h4>
-      <h3 className="faq__question-text">How do I get started?</h3>
-      <h4 className="faq__answer">
-        Fill out a 2 minute evaluation form get your personalized plan
-        instantly. Most exercises require no equipment, though some may require
-        equipment (resistance bands, steps, etc.), which can be substituted for
-        other items if unavailable.
-      </h4>
-      <h3 className="faq__question-text">
-        Will I get new exercises as I improve?
-      </h3>
-      <h4 className="faq__answer">
-        Yes! You can retake the evaluation whenever you feel ready to move onto
-        more advanced exercises. We recommend taking an evaluation every 2
-        weeks!
-      </h4>
-      <h3 className="faq__question-text">Is my data safe?</h3>
-      <h4 className="faq__answer">
-        Yes, AnkHeal does not collect sensitive medical data. Your exercise
-        lists and feedback is stored securly and used only to improve your
-        experience.
-      </h4>
-      <h3 className="faq__question-text">Do I still need a physiotherapist?</h3>
-      <h4 className="faq__answer">
-        AnkHeal is designed to complement—not replace—a health professional. For
-        any injury it is always recommended to see a licensed healthcare
-        provider for a complete and thorough evaluation.{" "}
-      </h4>
+      {displayAnswer === true ? (
+        <h4 className="faq__answer">
+          While AnkHeal focuses on ankle rehabilitation, many of the exercises
+          also support surrounding muscles and joints (calves, knees, etc.)
+          which can benefit overall mobility and athletic performance. The
+          health of surrounding muscles are taken into consideration when your
+          exercises are recommended.
+        </h4>
+      ) : null}
+      <div className="faq__question" onClick={handleDisplayAnswer}>
+        <h3 className="faq__question-text">How are the exercises selected?</h3>
+        <p className="faq__question-expand">+</p>
+      </div>
+      {displayAnswer === true ? (
+        <div className="faq__answer-full">
+          <h4 className="faq__answer">
+            Your responses in the evaluation form are used to match you with a
+            tailored list of recovery-focused exercises. The exercises are
+            selected from our database based on research, recovery best
+            practices, and real-world testing to support your progress safely
+            and effectively.{" "}
+          </h4>
+          <p className="faq__answer-warning">
+            ⚠️ All exercises are meant to challenge you. If any exercise causes
+            discomfort stop immediately or switch to a gentler variation
+          </p>
+        </div>
+      ) : null}
+
+      <div className="faq__question" onClick={handleDisplayAnswer}>
+        <h3 className="faq__question-text">
+          What does the subscription provide?
+        </h3>{" "}
+        <p className="faq__question-expand">+</p>
+      </div>
+      {displayAnswer === true ? (
+        <div className="faq__answer-full">
+          <ul className="faq__answer-list">
+            <li className="faq__answer-single-point">
+              Unlimited access to personalized ankle recovery plans
+            </li>
+            <li className="faq__answer-single-point">
+              A 7-day free trial for all subscription options
+            </li>
+            <li className="faq__answer-single-point">
+              Discounted yearly plan with early access to new features
+            </li>
+          </ul>
+          <Link to="/pricing" className="faq__plan-link">
+            See subscription plans
+          </Link>
+        </div>
+      ) : null}
+      <div className="faq__question" onClick={handleDisplayAnswer}>
+        <h3 className="faq__question-text">
+          If I cancel my subscription, will I lose everything?
+        </h3>{" "}
+        <p className="faq__question-expand">+</p>
+      </div>
+
+      {displayAnswer === true ? (
+        <h4 className="faq__answer">
+          You won't be able to take new evaluations, but you will have access to
+          your past exercise plans.
+        </h4>
+      ) : null}
+      <div className="faq__question" onClick={handleDisplayAnswer}>
+        <h3 className="faq__question-text">
+          Can I cancel my subscription anytime?
+        </h3>{" "}
+        <p className="faq__question-expand">+</p>
+      </div>
+
+      {displayAnswer === true ? (
+        <h4 className="faq__answer">
+          Yes, you can cancel from your account settings at any time. Email us
+          if you run into any issues.
+        </h4>
+      ) : null}
+      <div className="faq__question" onClick={handleDisplayAnswer}>
+        <h3 className="faq__question-text">How do I get started?</h3>
+        <p className="faq__question-expand">+</p>
+      </div>
+      {displayAnswer === true ? (
+        <h4 className="faq__answer">
+          Fill out a 2 minute evaluation form get your personalized plan
+          instantly. Most exercises require no equipment, though some may
+          require equipment (resistance bands, steps, etc.), which can be
+          substituted for other items if unavailable.
+        </h4>
+      ) : null}
+      <div className="faq__question" onClick={handleDisplayAnswer}>
+        <h3 className="faq__question-text">
+          Will I get new exercises as I improve?
+        </h3>{" "}
+        <p className="faq__question-expand">+</p>
+      </div>
+
+      {displayAnswer === true ? (
+        <h4 className="faq__answer">
+          Yes! You can retake the evaluation whenever you feel ready to move
+          onto more advanced exercises. We recommend taking an evaluation every
+          2 weeks!
+        </h4>
+      ) : null}
+      <div className="faq__question" onClick={handleDisplayAnswer}>
+        <h3 className="faq__question-text">Is my data safe?</h3>
+        <p className="faq__question-expand">+</p>
+      </div>
+      {displayAnswer === true ? (
+        <h4 className="faq__answer">
+          Yes, AnkHeal does not collect sensitive medical data. Your exercise
+          lists and feedback is stored securly and used only to improve your
+          experience.
+        </h4>
+      ) : null}
+      <div className="faq__question" onClick={handleDisplayAnswer}>
+        <h3 className="faq__question-text">
+          Do I still need a physiotherapist?
+        </h3>
+        <p className="faq__question-expand">+</p>
+      </div>
+      {displayAnswer === true ? (
+        <h4 className="faq__answer">
+          AnkHeal is designed to complement—not replace—a health professional.
+          For any injury it is always recommended to see a licensed healthcare
+          provider for a complete and thorough evaluation.{" "}
+        </h4>
+      ) : null}
       <p className="faq__contact-info">
         Still have questions? Contact us at{" "}
         <a href="mailto:ankhealinc@gmail.com">ankhealinc@gmail.com</a>
