673ac290624b36b8108ffbcec6d1f21ce3ffc2ce
Sumer Bhaidani <sumerbhaidani1@gmail.com>
2025-05-09 15:47:40 -0600

    Tablet Styling Complete - Review in Progress

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -28,62 +28,65 @@ function Footer() {
             Past Evaluations
           </Link>
         </section>
-        <h5 className="footer__section-title">Connect with us!</h5>
         <div className="footer__socials">
-          <a
-            href="https://www.instagram.com/ankheal"
-            className="footer__redirect"
-            target="_blank"
-            aria-label="Click here to navigate to AnkHeal's Instagram page"
-          >
-            <img
-              src={InstagramLogo}
-              alt="Instagram Logo"
-              className="footer__instagram"
-            />
-          </a>
-          <a
-            href="mailto:ankhealinc@gmail.com"
-            className="footer__redirect"
-            aria-label="Click here to email the AnkHeal team"
-          >
-            <svg
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-              className="footer__social-svg"
+          {" "}
+          <h5 className="footer__section-title">Connect with us!</h5>
+          <div className="footer__socials-logos">
+            <a
+              href="https://www.instagram.com/ankheal"
+              className="footer__redirect"
+              target="_blank"
+              aria-label="Click here to navigate to AnkHeal's Instagram page"
             >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M3.75 5.25L3 6V18L3.75 18.75H20.25L21 18V6L20.25 5.25H3.75ZM4.5 7.6955V17.25H19.5V7.69525L11.9999 14.5136L4.5 7.6955ZM18.3099 6.75H5.68986L11.9999 12.4864L18.3099 6.75Z"
-                className="footer__social-path"
+              <img
+                src={InstagramLogo}
+                alt="Instagram Logo"
+                className="footer__instagram"
               />
-            </svg>
-          </a>
-          <a
-            href="https://www.facebook.com/profile.php?id=61575089157050"
-            className="footer_redirect"
-            target="_blank"
-            aria-label="Click here to navigate to AnkHeal's Facebook page"
-          >
-            <img
-              src={FacebookLogo}
-              alt="Facebook Logo"
-              className="footer_facebook"
-            />
-          </a>
-          <a
-            href="https://x.com/ankheal"
-            className="footer_redirect"
-            target="_blank"
-            aria-label="Click here to navigate to AnkHeal's X page"
-          >
-            <img
-              src={TwitterLogo}
-              alt="Twitter Logo"
-              className="footer__twitter"
-            />
-          </a>
+            </a>
+            <a
+              href="mailto:ankhealinc@gmail.com"
+              className="footer__redirect"
+              aria-label="Click here to email the AnkHeal team"
+            >
+              <svg
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                className="footer__social-svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M3.75 5.25L3 6V18L3.75 18.75H20.25L21 18V6L20.25 5.25H3.75ZM4.5 7.6955V17.25H19.5V7.69525L11.9999 14.5136L4.5 7.6955ZM18.3099 6.75H5.68986L11.9999 12.4864L18.3099 6.75Z"
+                  className="footer__social-path"
+                />
+              </svg>
+            </a>
+            <a
+              href="https://www.facebook.com/profile.php?id=61575089157050"
+              className="footer_redirect"
+              target="_blank"
+              aria-label="Click here to navigate to AnkHeal's Facebook page"
+            >
+              <img
+                src={FacebookLogo}
+                alt="Facebook Logo"
+                className="footer_facebook"
+              />
+            </a>
+            <a
+              href="https://x.com/ankheal"
+              className="footer_redirect"
+              target="_blank"
+              aria-label="Click here to navigate to AnkHeal's X page"
+            >
+              <img
+                src={TwitterLogo}
+                alt="Twitter Logo"
+                className="footer__twitter"
+              />
+            </a>
+          </div>
         </div>
         <Link className="footer__logo" to="/">
           <img
