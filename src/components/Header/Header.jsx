c7f9a72cc6bc7c152231b444b267a9c9f3c3ed21
Sumer Bhaidani <sumerbhaidani1@gmail.com>
2025-05-08 21:03:09 -0600

    Header NavLink Conditional Render Complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,11 @@
 import LogoImage from "../../assets/logo/ankheal-logo.png";
 import { NavLink, Link } from "react-router-dom";
 import "./Header.scss";
+import { UserAuth } from "../../utils/AuthContext";
 
 function Header() {
+  const { session } = UserAuth();
+
   return (
     <header className="header" role="banner">
       <Link
@@ -19,38 +22,73 @@ function Header() {
           <h1 className="header__logo-name">AnkHeal</h1>
         </div>
       </Link>
-      <ul className="header__nav-bar">
-        <li className="header__nav-link">
-          <NavLink
-            to="/questionnaire"
-            className="header__nav-redirect"
-            aria-label="Click here to go to evalutation form"
-          >
-            Evaluation Form{" "}
-          </NavLink>
-        </li>
+      {session == null ? (
+        <ul className="header__nav-bar">
+          <li className="header__nav-link">
+            <NavLink
+              to="/faq"
+              className="header__nav-redirect"
+              aria-label="Click here to go to evalutation form"
+            >
+              FAQ{" "}
+            </NavLink>
+          </li>
 
-        <li className="header__nav-link">
-          <NavLink
-            to="/evaluation"
-            className="header__nav-redirect"
-            aria-label="View Past Evaluations here"
-          >
-            Past Evaluations
-          </NavLink>
-        </li>
+          <li className="header__nav-link">
+            <NavLink
+              to="/pricing"
+              className="header__nav-redirect"
+              aria-label="View Past Evaluations here"
+            >
+              Pricing
+            </NavLink>
+          </li>
 
-        <li className="header__nav-link">
-          {" "}
-          <NavLink
-            to="/user"
-            className="header__nav-redirect"
-            aria-label="Account dashboard or login"
-          >
-            My Account{" "}
-          </NavLink>
-        </li>
-      </ul>
+          <li className="header__nav-link">
+            {" "}
+            <NavLink
+              to="/user"
+              className="header__nav-redirect"
+              aria-label="Account dashboard or login"
+            >
+              My Account{" "}
+            </NavLink>
+          </li>
+        </ul>
+      ) : (
+        <ul className="header__nav-bar">
+          <li className="header__nav-link">
+            <NavLink
+              to="/questionnaire"
+              className="header__nav-redirect"
+              aria-label="Click here to go to evalutation form"
+            >
+              Evaluation Form{" "}
+            </NavLink>
+          </li>
+
+          <li className="header__nav-link">
+            <NavLink
+              to="/evaluation"
+              className="header__nav-redirect"
+              aria-label="View Past Evaluations here"
+            >
+              Past Evaluations
+            </NavLink>
+          </li>
+
+          <li className="header__nav-link">
+            {" "}
+            <NavLink
+              to="/user"
+              className="header__nav-redirect"
+              aria-label="Account dashboard or login"
+            >
+              My Account{" "}
+            </NavLink>
+          </li>
+        </ul>
+      )}
     </header>
   );
 }
