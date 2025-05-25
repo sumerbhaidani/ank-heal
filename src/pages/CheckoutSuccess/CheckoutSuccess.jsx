e86784e6f4fc83aa2f3dfae4fb6068b866c18aab
Sumer Bhaidani <sumerbhaidani1@gmail.com>
2025-05-25 17:40:44 -0600

    Checkout Success Redesign Complete

--- src/pages/CheckoutSuccess/CheckoutSuccess.jsx
+++ src/pages/CheckoutSuccess/CheckoutSuccess.jsx
@@ -1,24 +1,30 @@
 import "./CheckoutSuccess.scss";
+import Header from "../../components/Header/Header.jsx";
+import Footer from "../../components/Footer/Footer.jsx";
 
 function CheckoutSuccess() {
   return (
-    <div className="checkout-success">
-      <h2 className="checkout-success__header">Payment Successful!</h2>
-      <h3 className="checkout-success__message">
-        You now have access to all of AnkHeal's features! <br />
-        Please contact us at{" "}
-        <a
-          href="mailto:ankhealinc@gmail.com"
-          className="checkout-success__email"
-        >
-          ankhealinc@gmail.com
-        </a>{" "}
-        if you have any questions!
-      </h3>
-      <h4 className="checkout-success__final">
-        Good luck on your recovery journey!
-      </h4>
-    </div>
+    <>
+      <Header />
+      <div className="checkout-success">
+        <h2 className="checkout-success__header">Payment Successful!</h2>
+        <h3 className="checkout-success__message">
+          You now have access to all of AnkHeal's features! <br />
+          Please contact us at{" "}
+          <a
+            href="mailto:ankhealinc@gmail.com"
+            className="checkout-success__email"
+          >
+            ankhealinc@gmail.com
+          </a>{" "}
+          if you have any questions!
+        </h3>
+        <h4 className="checkout-success__final">
+          Good luck on your recovery journey!
+        </h4>
+      </div>
+      <Footer />
+    </>
   );
 }
 
