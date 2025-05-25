49fc1c2a6bc5c80c2edacffa37c431c99befbb8d
Sumer Bhaidani <sumerbhaidani1@gmail.com>
2025-05-25 11:02:45 -0600

    SVG Import Success

--- src/pages/LoginDashboard/LoginDashboard.jsx
+++ src/pages/LoginDashboard/LoginDashboard.jsx
@@ -3,6 +3,9 @@ import { UserAuth } from "../../utils/AuthContext";
 import "./LoginDashboard.scss";
 import { useNavigate } from "react-router-dom";
 import HeaderOnlyLogo from "../../components/HeaderOnlyLogo/HeaderOnlyLogo";
+import ClockIcon from "../../assets/icons/clock-icon.svg";
+import EvalIcon from "../../assets/icons/evaluation-icon.svg";
+import ResultsIcon from "../../assets/icons/past-results-icon.svg";
 
 function LoginDashboard({ portalLink }) {
   const param = useParams();
@@ -23,19 +26,21 @@ function LoginDashboard({ portalLink }) {
   return (
     <div className="dashboard">
       {" "}
-      <HeaderOnlyLogo />
       <div className="dashboard__main">
         <section className="dashboard__heading">
-          <h2 className="dashboard__welcome">
-            Welcome {session?.user?.user_metadata?.name}!
-          </h2>
+          <HeaderOnlyLogo />
+
           <p onClick={handleSignOut} className="dashboard__log-out">
             Log Out
           </p>
         </section>
+        <h2 className="dashboard__welcome">
+          Welcome {session?.user?.user_metadata?.name}!
+        </h2>
         {/* one card customer portal - one card progress tracker - one card when last evaluation taken and take next evaluation */}
-        <div className="dashboard__card">Take an Evaluation Here!</div>
         <div className="dashboard__card">Access your Past Evaluations</div>
+        <div className="dashboard__card">Recommended Exercise Inter</div>
+        <div className="dashboard__card">Take an Evaluation Here!</div>
         <a
           href={`${portalLink}?prefilled_email=${session.user.email}`}
           className="dashboard__customer-portal"
