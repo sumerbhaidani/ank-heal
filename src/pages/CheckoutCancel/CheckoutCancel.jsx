0cf3a7981959a8a7bf7ce6e61f8c42a6f2a1677f
Sumer Bhaidani <sumerbhaidani1@gmail.com>
2025-05-25 17:42:22 -0600

    Checkout Cancel Complete

--- src/pages/CheckoutCancel/CheckoutCancel.jsx
+++ src/pages/CheckoutCancel/CheckoutCancel.jsx
@@ -1,25 +1,32 @@
 import "./CheckoutCancel.scss";
 import { Link } from "react-router";
+import Header from "../../components/Header/Header.jsx";
+import Footer from "../../components/Footer/Footer.jsx";
 
 function CheckoutCancel() {
   return (
-    <div className="checkout-cancel">
-      <h2 className="checkout-cancel__header">Payment Cancelled!</h2>
-      <h3 className="checkout-cancel__message">
-        It happens - whether it's a change of mind, or technical issue.
-      </h3>
-      <h4 className="checkout-cancel__option">
-        If there was an error during the payment process, select your plan and
-        try again{" "}
-        <Link to="/pricing" className="checkout-cancel__link">
-          here
-        </Link>
-        .
-      </h4>
-      <h4 className="checkout-cancel__option">
-        To learn more about AnkHeal, click one of the links in the header above.
-      </h4>
-    </div>
+    <>
+      <Header />
+      <div className="checkout-cancel">
+        <h2 className="checkout-cancel__header">Payment Cancelled!</h2>
+        <h3 className="checkout-cancel__message">
+          It happens - whether it's a change of mind, or technical issue.
+        </h3>
+        <h4 className="checkout-cancel__option">
+          If there was an error during the payment process, select your plan and
+          try again{" "}
+          <Link to="/pricing" className="checkout-cancel__link">
+            here
+          </Link>
+          .
+        </h4>
+        <h4 className="checkout-cancel__option">
+          To learn more about AnkHeal, click one of the links in the header
+          above.
+        </h4>
+      </div>
+      <Footer />
+    </>
   );
 }
 export default CheckoutCancel;
