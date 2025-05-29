13719f0fca65ae4d1930d85de94f3a4476917353
Sumer Bhaidani <sumerbhaidani1@gmail.com>
2025-05-28 21:11:37 -0600

    Loading State Add

--- src/components/PlanValidation/PlanValidation.jsx
+++ src/components/PlanValidation/PlanValidation.jsx
@@ -5,12 +5,13 @@ import { UserAuth } from "../../utils/AuthContext";
 
 function PlanValidation({ baseUrl }) {
   const [userPlan, setUserPlan] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const { session } = UserAuth();
   async function checkPlan() {
     try {
       const response = await axios.get(`${baseUrl}/user/${session.user.id}`);
-      console.log(response.data[0].subscription_status);
+      setLoading(false);
       if (response.data[0].subscription_status === "active") {
         setUserPlan(true);
       } else {
@@ -27,7 +28,9 @@ function PlanValidation({ baseUrl }) {
 
   return (
     <div className="plan-validation">
-      <h2 className="plan-validation__header">Subscription Needed</h2>
+      <h2 className="plan-validation__header">
+        {loading === true ? "Verifying Subscription" : "Subscription Needed"}
+      </h2>
       <h3 className="plan-validation__message">
         To access this part of the site, you need to have an active
         subscription. If you don't have an active subscription, you will see a
