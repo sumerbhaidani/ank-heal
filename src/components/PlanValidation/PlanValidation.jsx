5796d166a9e9262338cad40b7ebbe504c3db911f
Sumer Bhaidani <sumerbhaidani1@gmail.com>
2025-05-29 22:15:24 -0600

    Eval Form Width Fix

--- src/components/PlanValidation/PlanValidation.jsx
+++ src/components/PlanValidation/PlanValidation.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { UserAuth } from "../../utils/AuthContext";
 import { Link } from "react-router";
+import HeaderOnlyLogo from "../HeaderOnlyLogo/HeaderOnlyLogo";
 
 function PlanValidation({ baseUrl }) {
   const [userPlan, setUserPlan] = useState(false);
@@ -34,45 +35,50 @@ function PlanValidation({ baseUrl }) {
   }, []);
 
   return isVisible === true ? (
-    <div className="plan-validation">
-      <h2
-        className={`plan-validation__header ${
-          loading === false ? "plan-validation--underline" : null
-        }`}
-      >
-        {loading === true ? "Verifying Subscription..." : "Subscription Status"}
-      </h2>
-      <h3 className="plan-validation__message">
-        To access this part of the site, you need to have an active
-        subscription.{" "}
-        {loading === false ? "View your subscription status below." : null}
-      </h3>
-      {loading === false ? (
-        userPlan === true ? (
-          <>
-            <h4 className="plan-validation__instruction">
-              Click the button below to proceed.
-            </h4>
-            <div
-              className="plan-validation__hide-button"
-              onClick={handleIsVisible}
-            >
-              Begin evaluation
-            </div>
-          </>
-        ) : (
-          <>
-            <h4 className="plan-validation__instruction">
-              Click the button below to sign up for a subscription plan to
-              access this content.
-            </h4>
-            <Link to="/pricing" className="plan-validation__redirect">
-              Select a subscription plan
-            </Link>
-          </>
-        )
-      ) : null}
-    </div>
+    <>
+      <div className="plan-validation">
+        <HeaderOnlyLogo />
+        <h2
+          className={`plan-validation__header ${
+            loading === false ? "plan-validation--underline" : null
+          }`}
+        >
+          {loading === true
+            ? "Verifying Subscription..."
+            : "Subscription Status"}
+        </h2>
+        <h3 className="plan-validation__message">
+          To access this part of the site, you need to have an active
+          subscription.{" "}
+          {loading === false ? "View your subscription status below." : null}
+        </h3>
+        {loading === false ? (
+          userPlan === true ? (
+            <>
+              <h4 className="plan-validation__instruction">
+                Click the button below to proceed.
+              </h4>
+              <div
+                className="plan-validation__hide-button"
+                onClick={handleIsVisible}
+              >
+                Begin evaluation
+              </div>
+            </>
+          ) : (
+            <>
+              <h4 className="plan-validation__instruction">
+                Click the button below to sign up for a subscription plan to
+                access this content.
+              </h4>
+              <Link to="/pricing" className="plan-validation__redirect">
+                Select a subscription plan
+              </Link>
+            </>
+          )
+        ) : null}
+      </div>
+    </>
   ) : null;
 }
 
