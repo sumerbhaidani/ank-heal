41fa4d8bc7caae5bf58870f45c5ab0ab71ca46e3
Sumer Bhaidani <sumerbhaidani1@gmail.com>
2025-05-29 21:04:13 -0600

    Loading Status Edit

--- src/components/PlanValidation/PlanValidation.jsx
+++ src/components/PlanValidation/PlanValidation.jsx
@@ -7,6 +7,7 @@ import { Link } from "react-router";
 function PlanValidation({ baseUrl }) {
   const [userPlan, setUserPlan] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [isVisible, setIsVisible] = useState(true);
 
   const { session } = UserAuth();
   async function checkPlan() {
@@ -24,33 +25,45 @@ function PlanValidation({ baseUrl }) {
     }
   }
 
+  function handleIsVisible() {
+    setIsVisible(false);
+  }
+
   useEffect(() => {
     checkPlan();
   }, []);
 
-  return (
+  return isVisible === true ? (
     <div className="plan-validation">
-      <h2 className="plan-validation__header">
-        {loading === true ? "Verifying Subscription..." : "Subscription Needed"}
+      <h2
+        className={`plan-validation__header ${
+          loading === false ? "plan-validation--underline" : null
+        }`}
+      >
+        {loading === true ? "Verifying Subscription..." : "Subscription Status"}
       </h2>
       <h3 className="plan-validation__message">
         To access this part of the site, you need to have an active
-        subscription.
-        <br />
+        subscription.{" "}
+        {loading === false ? "View your subscription status below." : null}
+      </h3>
+      <h4 className="plan-validation__instruction">
         {userPlan === true
           ? "Click the button below to proceed."
-          : "You must have an active subscription to access this part of the site. Click the button below to sign up for a subscription plan to access this content."}{" "}
-      </h3>
+          : "Click the button below to sign up for a subscription plan to access this content."}{" "}
+      </h4>
       {/* <div className="plan-validation__redirect"> */}
       {userPlan === true ? (
-        <div className="plan-validation__hide-button">Begin evaluation</div>
+        <div className="plan-validation__hide-button" onClick={handleIsVisible}>
+          Begin evaluation
+        </div>
       ) : (
         <Link to="/pricing" className="plan-validation__redirect">
           Select a subscription plan
         </Link>
       )}
     </div>
-  );
+  ) : null;
 }
 
 export default PlanValidation;
